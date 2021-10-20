d2624b63d2b7d94b06f06feed6df99e287c8d798
SubhamChoudhury <subhamchoudhury2408@gmail.com>
2021-10-20 23:59:02 +0530

    feat : add error page with UI

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Route path="/about" component={About}></Route>
     <Route path="/contact" component={Contact}></Route>
     <Route path="/service" component={Service}></Route>
-    <Route path="/error" component={Error}></Route>
+    <Route component={Error}></Route>
   <Home/>
   </Switch>
   </>
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Navbar from '../navbar'
+import Error404 from '../Error404'
+import Footer from '../Footer'
 
 const Error = () => {
     return (
-        <div>
-           <h1>Error</h1> 
-        </div>
+        <>
+            <Navbar />
+            <Error404 />
+            <Footer />
+        </>
     )
 }
 
